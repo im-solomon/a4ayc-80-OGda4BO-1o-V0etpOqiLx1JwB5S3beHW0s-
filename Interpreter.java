f2f623c0b89ebfc12b4a0436ba2b515b44a6d82f
Solomon <47408821+im-solomon@users.noreply.github.com>
2019-11-13 15:48:28 +0900

    Add files via upload

--- Interpreter.java
+++ Interpreter.java
@@ -2,27 +2,63 @@ package generated;
 import org.antlr.v4.runtime.*;
 import java.util.*;
 
+class StateMap {																				//state 저장 클래스
+	private static HashMap<String, Integer> stateMapVar = new HashMap<String, Integer>();		
+	public StateMap(){																			//state 초기값
+		stateMapVar.put("x", 7);
+		stateMapVar.put("y", 1);
+	}
+	public void SetState(String strVar, Integer intVar) {										//state key value 세팅
+		stateMapVar.put(strVar, intVar);
+	}
+	public HashMap<String, Integer> GetStateMapObject(){										//state 객체 리턴
+		return stateMapVar;
+	}
+}
 
 public class Interpreter {	
-    public static void main(String[] args) throws Exception {
-    	StateMap vars =  new StateMap();
+    
+	public static void main(String[] args) throws Exception {
+    	StateMap vars =  new StateMap();																//state 객체 생성
+    	System.out.println("계산기 시작");
+    	while(true) {	
+    		System.out.println("번호 선택,   1.State 출력   2.State 입력   3.수식계산   4.종료");
     	
-    	vars.SetState("z", 1);
-    	vars.SetState("x", 2);
+    		int num = new Scanner(System.in).nextInt();	
+    		if(num == 1) {
+    			System.out.println("현재 States:");
+    			for(String key : vars.GetStateMapObject().keySet()) {									//모든 state key value 출력
+    				System.out.println("key=value: "+key + " = "+vars.GetStateMapObject().get(key));	
+    			}
+    			
+    			System.out.println();
+    		}else if(num == 2){																			//state 생성 및 substitute
+    			
+    			System.out.println("State 입력:");
+    			System.out.println("State key:");
+    			String key = new Scanner(System.in).nextLine();
+    			System.out.println("State value:");
+    			Integer value = new Scanner(System.in).nextInt();
+    			vars.SetState(key, value);
+    			
+    		}else if(num == 3){																			//수식 계산
+    			System.out.println("수식 입력:");
+    			CharStream input = CharStreams.fromString(new Scanner(System.in).nextLine());			//수식을 문자열로 받음
+    			WHILELexer lexer = new WHILELexer(input);												//문자열을 lexer rule 기준으로 나눔
+    			CommonTokenStream tokens = new CommonTokenStream(lexer);								//의미있는 단어를 기준 최소단위로 나눔				
+    			WHILEParser parser = new WHILEParser(tokens);											//parser rule 기준으로 ast를 생성한다
+            
+    			parser.memory.putAll(vars.GetStateMapObject());											//parser의 멤버객체에 state 객체 전달
+    			
+    			System.out.println(parser.program().aexpr().sv);										//최종 결과인 리턴 값 출력
+    			System.out.println();
+    			
+    		}else {
+    			break;
+    		}  	
     	
-    	System.out.println(vars.GetStateMapObject().get("x"));
-    	System.out.println(vars.GetStateMapObject().get("y"));
-    	System.out.println(vars.GetStateMapObject().get("z"));
+    	}
     	
-    	
-    	System.out.println("계산 형식 입력 ex)x+(x+z):");
-    	@SuppressWarnings("resource")
-		CharStream input = CharStreams.fromString(new Scanner(System.in).nextLine());
-        WHILELexer lexer = new WHILELexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        WHILEParser parser = new WHILEParser(tokens);
-        parser.memory.putAll(vars.GetStateMapObject());
-    	System.out.println(parser.program().aexpr().sv);
     
     }
 }
--- WHILE.g4
+++ WHILE.g4
@@ -3,45 +3,45 @@
  */
 grammar WHILE;
 
-@header {
+@header {											//자동 생성되는 분석기 생성 폴더
 package generated;
 }
-@parser::members{
+
+@parser::members{									//antlr안에서 사용될 자바 객체
 	public java.util.HashMap<String, Integer> memory = new java.util.HashMap<String, Integer>();
 	
 }
-program: aexpr;
-
-aexpr returns [int sv]
-	: 	N = additionExp {$sv = $N.sv;}
+program: aexpr;										//별칭
+																				//parser rule 시작, lexer rule로 나눠진 토큰들의 의미 파악
+aexpr returns [int sv]															//aexpr의 리턴 타입 정의
+	: 	N = additionExp {$sv = $N.sv;}											//모든 수식이 계산 된 리턴 값 받기
 	;
-additionExp returns [int sv]
-	: 	a1 = multiplyExp		{$sv = $a1.sv;}
+additionExp returns [int sv]													//+ - 문자열의 의미 정의, 리턴 타입 정의
+	: 	a1 = multiplyExp		{$sv = $a1.sv;}									//multiplyExp 규칙 먼저 수행 후 리턴 값에 대해 + - 정의
 		(	'+' a2=multiplyExp 	{$sv += $a2.sv;} 
         | 	'-' a2=multiplyExp 	{$sv -= $a2.sv;}	
-		)*
+		)*																		//zero or more
 	;
 	
-multiplyExp returns [int sv]
-	:   m1 = atomExp       {$sv =  $m1.sv;}
+multiplyExp returns [int sv]													// * / 문자열의 의미 정의, 리턴 타입 정의
+	:   m1 = atomExp       {$sv =  $m1.sv;}										// 사칙연산 외의 문자에 대한 해석이 선행되어야 함
         ( '*' m2=atomExp {$sv *= $m2.sv;} 
         | '/' m2=atomExp {$sv /= $m2.sv;}
-        )*  
+        )*  																	//zero or more
 	;
-atomExp returns [int sv]
-	: 	n=Number				{$sv = Integer.parseInt($n.text);}
-	|	i=Identifier			{$sv = memory.get($i.text);}
-	|	'(' exp=additionExp ')'	{$sv = $exp.sv;}
+atomExp returns [int sv]														//사칙연산 외의 문자에 대한 구문 해석
+	: 	n=Number				{$sv = Integer.parseInt($n.text);}				//토큰이 Number라면 정수로 변환
+	|	i=Identifier			{$sv = memory.get($i.text);}					//토큰이 Identifier라면 State에서 참조함
+	|	'(' exp=additionExp ')'	{$sv = $exp.sv;}								//괄호 발견되면 additionExp 규칙에서 다시 시작(리커젼)
 	;
 
-	
-//렉서 룰, syntax 룰
+																				//lexer rule 시작, 의미있는 단위로 나누는 규칙
 Identifier
-	:	('a'..'z' | 'A'..'Z' | '_') ('a'..'z' | 'A'..'Z' | '_' | '0'..'9')* //앞자리 대소문자,언더바만 허용 뒷자리부터는 숫자까지 허용
+	:	('a'..'z' | 'A'..'Z' | '_') ('a'..'z' | 'A'..'Z' | '_' | '0'..'9')* 	//한자리 이상의 변수, 앞자리 대소문자/언더바만 허용, 뒷자리부터는 숫자까지 허용(zero or more)
 	;
 Number
-	:	('0'..'9')+ //한자리 이상의 정수만 허용
+	:	('0'..'9')+ 															//한자리 이상의 정수만 허용 (one or more)
 	;
 
-WS : [ \t\r\n]+ -> skip ; 
+WS : [ \t\r\n]+ -> skip ; 														//공백,탭,엔터 값 무시함
 
